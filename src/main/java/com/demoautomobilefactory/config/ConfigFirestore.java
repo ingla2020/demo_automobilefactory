d7779ce439de9681c022dd69dfbe9b16935dbdf8
Ing Leonardo Arevalo <leonardojosearevalo@gmail.com>
2021-03-16 22:41:27 -0300

    primer commit test

--- src/main/java/com/demoautomobilefactory/config/ConfigFirestore.java
+++ src/main/java/com/demoautomobilefactory/config/ConfigFirestore.java
@@ -1,8 +1,13 @@
 package com.demoautomobilefactory.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
 
@@ -14,11 +19,22 @@ public class ConfigFirestore {
     public String fire;
 
     @Bean
-    public Firestore firestore() {
+    public Firestore firestore() throws IOException {
+
+    	FileInputStream serviceAccount =
+    			  new FileInputStream("src/main/resources/fir-testing-626a8-firebase-adminsdk-p84d2-2a61ce69b9.json");
+    	
         FirestoreOptions firestoreOptions =
                 FirestoreOptions.getDefaultInstance().toBuilder()
-                        .setProjectId(fire).build();
+                        .setProjectId(fire)
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
         return firestoreOptions.getService();
+        
+        
+        
+        
+        
     }	
 	
 }
