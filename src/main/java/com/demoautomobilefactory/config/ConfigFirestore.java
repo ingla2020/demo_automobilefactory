46e29975111d67bf0db0bf2429839f8676652aa3
Ing Leonardo Arevalo <leonardojosearevalo@gmail.com>
2021-03-18 13:38:57 -0300

    junit

--- src/main/java/com/demoautomobilefactory/config/ConfigFirestore.java
+++ src/main/java/com/demoautomobilefactory/config/ConfigFirestore.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 @Configuration
 public class ConfigFirestore {
@@ -21,17 +24,43 @@ public class ConfigFirestore {
     @Bean
     public Firestore firestore() throws IOException {
 
-//    	FileInputStream serviceAccount =
-  //  			  new FileInputStream("src/main/resources/fir-testing-626a8-firebase-adminsdk-p84d2-2a61ce69b9.json");
+    	/*
+    	FileInputStream serviceAccount =
+    			  new FileInputStream("src/main/resources/fir-testing-626a8-firebase-adminsdk-p84d2-2a61ce69b9.json");
     	
         FirestoreOptions firestoreOptions =
                 FirestoreOptions.getDefaultInstance().toBuilder()
                         .setProjectId(fire)
-                       // .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                         .build();
         return firestoreOptions.getService();
         
-        
+*/
+    	
+/*    	
+    	// Use a service account
+    	InputStream serviceAccount = new FileInputStream("src/main/resources/fir-testing-626a8-firebase-adminsdk-p84d2-2a61ce69b9.json");
+    	GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+    	FirebaseOptions options = new FirebaseOptions.Builder()
+    	    .setCredentials(credentials)
+    	    .build();
+    	FirebaseApp.initializeApp(options);
+
+    	Firestore db = FirestoreClient.getFirestore();    	
+  */  	
+    	FileInputStream serviceAccount =
+    			  new FileInputStream("src/main/resources/fir-testing-626a8-firebase-adminsdk-p84d2-2a61ce69b9.json");
+
+    			FirebaseOptions options = new FirebaseOptions.Builder()
+    			  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+    			  .setDatabaseUrl("https://fir-testing-626a8.firebaseio.com")
+    			  .build();
+
+    			FirebaseApp.initializeApp(options);
+    			
+    			Firestore db = FirestoreClient.getFirestore();
+    			
+    			return db;
         
         
         
